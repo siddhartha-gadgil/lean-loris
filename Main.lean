e992210c2548723917c2b2b3dfff5337cd89e09e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-01-03 10:07:42 +0530

    initialized project

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanLoris
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,6 @@
+import Lake
+open Lake DSL
+
+package «lean-loris» {
+  -- add configuration options here
+}
