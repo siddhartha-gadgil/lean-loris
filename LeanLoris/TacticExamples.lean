d7516e6f3abfc0507b6097d6376b9fcfed166a2c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-02-16 15:19:57 +0530

    nat-rec example in steps

--- LeanLoris/TacticExamples.lean
+++ LeanLoris/TacticExamples.lean
@@ -25,10 +25,30 @@ set_option maxHeartbeats 100000000
 def recTest(f: Nat → Nat) :=
   let p := (∀ x: Nat, f (x + 1) = f x) → (∀ x: Nat, f x = f 0)
   let hyp := ∀ x: Nat, f (x + 1) = f x
-  let seek := evolve! ^[pi-goals, rfl, eq-closure, nat-rec, app] 
-            %[p, hyp → f 0 = f 0, hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0),
+  let seek := evolve! ^[pi-goals, rfl, eq-closure, nat-rec, app, binop] 
+            %[p, 
+            hyp → f 0 = f 0, 
+            hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0),
             hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f n),
             hyp → f 0 = f 0 → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0) → ∀ (n : Nat), f n = f 0] %{(p, 0)} 2 5000
+  let step := hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0)
+  let stepPf : step := fun h n ih => Eq.trans (h n) ih
+  let seek2 := evolve! ^[pi-goals, rfl, eq-closure, nat-rec, app, binop] 
+            %[p, 
+            hyp → f 0 = f 0, 
+            hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0),
+            hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f n),
+            hyp → f 0 = f 0 → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0) → ∀ (n : Nat), f n = f 0] %{(p, 0), (stepPf, 1)} 2 5000
+  let base : Prop := hyp → f 0 = f 0
+  let basePf : base := fun hyp => Eq.refl (f 0)
+  let recFn := fun _ : hyp => natRec (fun n => f n = f 0)
+  let seek3 := evolve! ^[pi-goals, binop] %[p, 
+            hyp → f 0 = f 0, 
+            hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0),
+            hyp → (∀ (n : Nat), f n = f 0 → f (n + 1) = f n),
+            hyp → f 0 = f 0 → (∀ (n : Nat), f n = f 0 → f (n + 1) = f 0) → ∀ (n : Nat), f n = f 0] %{(p, 0), (basePf, 0), (recFn, 0), (stepPf, 0)} 2 5000
+  let pf : p := fun h => (recFn h) (basePf h) (stepPf h)
   ()
+
 
 #check @Nat.brecOn
