5762a4936f4d91b5b747d5f12e26f1c13e68f1d2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-02-25 09:14:10 +0530

    fixed point before iteration

--- LeanLoris/CompiledCzSl.lean
+++ LeanLoris/CompiledCzSl.lean
@@ -67,12 +67,12 @@ def goals : TermElabM (Array Expr) := do
 
 def evolve1: TermElabM EvolutionM := do
             let step := initEv ++ nameAppl ++ nameBinOp ++ eqIsles
-            let ev  := step.iterate.fixedPoint.andThenM (logResults <| ←  goals)
+            let ev  := step.evolve.andThenM (logResults <| ←  goals)
             return ev 3 6000 initData
 
 def evolve2: TermElabM EvolutionM := do
             let step := initEv ++ eqClosure
-            let ev  := step.iterate.fixedPoint.andThenM (logResults <| ←  goals)
+            let ev  := step.evolve.andThenM (logResults <| ←  goals)
             return ev 1 6000 initData
 
 def evolve : TermElabM EvolutionM := do
