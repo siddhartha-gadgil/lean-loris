d03ddaf9c1e83ddb6b1978722b688d9f41f1477e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-03-02 11:29:07 +0530

    testing helpers for ML

--- premise_predictor.py
+++ premise_predictor.py
@@ -29,23 +29,23 @@ def normalize(l):
         return floatlist(l)
     return [float(x) / float(total) for x in l] 
 
-statements = [floatlist(row) for row in types][:-1000]
-proofs = [normalize(row) for row in terms][:-1000]
+statements = [floatlist(row) for row in types]
+proofs = [normalize(row) for row in terms]
 
 model = keras.Sequential([
     keras.Input(shape=(dim,), name="types"),
     layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_1"),
     layers.Dense(32, activation="relu", kernel_initializer='glorot_normal',  name="dense_2"),
-    layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_3"),
-    layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_4"),
+    # layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_3"),
+    # layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_4"),
     layers.Dense(dim, activation="softmax", name="predictions"),
     ]
     )
 
-statements = tf.convert_to_tensor(statements, dtype=tf.float32)
-proofs = tf.convert_to_tensor(proofs, dtype=tf.float32)
-print(statements.shape)
-print(proofs.shape)
+statements_tensor = tf.convert_to_tensor(statements[:-1000], dtype=tf.float32)
+proofs_tensor = tf.convert_to_tensor(proofs[:-1000], dtype=tf.float32)
+print(statements_tensor.shape)
+print(proofs_tensor.shape)
 
 model.compile(
     optimizer=keras.optimizers.Adam(),  # Optimizer
@@ -59,10 +59,10 @@ print('Built tensors')
 
 print("Fit model on training data")
 history = model.fit(
-    statements,
-    proofs,
+    statements_tensor,
+    proofs_tensor,
     batch_size=64,
-    epochs=256
+    epochs=1024
     # We pass some validation for
     # monitoring validation loss and metrics
     # at the end of each epoch
@@ -73,3 +73,31 @@ print("Done training")
 print(history.history)
 
 print(model.summary()) 
+
+def namevec(name):
+    return tf.convert_to_tensor([[1.0 if x == name else 0 for x in names]], dtype=tf.float32)
+
+def sort_weighted(weights):
+    def sort_key(x):
+        return weights[x]
+    l = list(range(len(names)))
+    l.sort(key=sort_key, reverse=True)
+    ns = [names[x] for x in l]
+    w = list(weights).copy()
+    w.sort(reverse=True)
+    return [(ns[i], w[i]) for i in range(len(names))]
+
+def name_prediction_weights(name):
+    return sort_weighted(model.predict(namevec(name))[0])
+
+def index_prediction(index):
+    stat = statements[index]
+    stat_tensor = tf.convert_to_tensor([stat], dtype=tf.float32)
+    prediction = model.predict(stat_tensor)[0]
+    return sort_weighted(stat), sort_weighted(proofs[index]), sort_weighted(prediction)
+
+def name_weight(l, name):
+    for (n, w) in l:
+        if n == name:
+            return w
+    return 0.0
