ce9312889cace7d86c0efe728e097eda21011480
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-03-04 18:41:04 +0530

    interpreted cherry-picked  Czech-Slovak Oly

--- LeanLoris/InterpCzSlOly.lean
+++ LeanLoris/InterpCzSlOly.lean
@@ -32,31 +32,16 @@ def exploreProofs(ax1 : ∀ a b : M, (a * b) * b = a)(ax2 : ∀ a b : M, a * (a 
                   let lem5! := (m * n) * m = n
                   let lem6! := ((m * n) * m) * m = n * m
                   let thm! := m * n = n * m
-                  -- let seek123 := evolve! ev![app, name-app] exp![lem1!, lem2!, lem3!] exp!{(ax1, 0), (ax2, 0), (m, 0), (n, 0)} name!{(mul, 0)} 5 1000
-                  -- let seekmn := evolve! ev![app, name-app] exp![m * n] exp!{(m, 0), (n, 0)} name!{(mul, 0)} 5 1000
                   let step1a := evolve! ev![app, name-app, name-binop, binop] exp![lem1!, lem2!, lem3!] exp!{(ax1, 0), (ax2, 0), (m, 0), (n, 0), (m *n, 0)} name!{(mul, 0), (Eq, 0)} 3 6000
                   let ⟨⟨lem1, w1⟩, ⟨lem2, w2⟩, ⟨lem3, w3⟩, _⟩ := step1a
                   let step1b := evolve! ev![eq-isles, name-binop] exp![lem4!] exp!{(m, 0), (n, 0), (m *n, 0), (lem1, 1), (lem2, 1), (lem3, 1)} name!{(mul, 0)} 3 6000
                   let ⟨⟨lem4, w4⟩, _⟩ := step1b
-                  let x := mul (HMul.hMul m n) ((HMul.hMul m n) * n)
-                  let y := HMul.hMul (HMul.hMul m n) (m * n * n)
-                  let z := HMul.hMul (HMul.hMul m n)  ((HMul.hMul m n) * n) 
-                  let check : (x = n) = lem2! := by rfl
-                  let check2 : x = y := by rfl
-                  let h1 := hash! x
-                  let h2 := hash! y
-                  let h0 := hash! z
-                  let h3 := hash! ((HMul.hMul m n) * n)
-                  let h4 := hash! (m * n * n)
-                  let h5 := hashv! mul m n
-                  let h6 := hash! m * n
-                  let h7 := hashv! (HMul.hMul m n)
-                  let step2 := evolve! ev![eq-closure] exp![lem5!] exp!{(m, 0), (n, 0), (m *n, 0), (lem2, 0), (lem4, 0)} name!{(mul, 0)} 1 6000
-                  let ⟨⟨lem5, w4⟩, _⟩ := step2
-                  -- let seek5 := evolve! ev![name-binop, eq-closure exp![n, (m * n) * ((m * n) * n)]] exp![m * n * n, (m * n) * ((m * n) * n), lem2!, lem4!, lem5!] exp!{(m, 0), (n, 0), (m *n, 0), (lem2, 1), (lem4, 3)} !{(mul, 0)} 4 2000
-                  -- seek5
-                  (h1, h2)
-             
-#check HMul.hMul
+                  let step2 := evolve! ev![eq-closure, name-binop] exp![lem5!] exp!{(m, 0), (n, 0), (m *n, 0),  (lem2, 1), (lem4, 3)} name!{(mul, 0)} 2 6000
+                  let ⟨⟨lem5, w5⟩, _⟩ := step2
+                  let step3 := evolve! ev![eq-isles, name-binop] exp![lem6!] exp!{(m, 0), (n, 0), (m *n, 0), (lem5, 1)} name!{(mul, 0)} 3 6000
+                  let ⟨⟨lem6, w6⟩, _⟩ := step3
+                  let step7 := evolve! ev![eq-closure, name-binop] exp![thm!] exp!{(m, 0), (n, 0), (m *n, 0),  (lem3, 1), (lem6, 1)} name!{(mul, 0)} 1 6000  
+                  let ⟨⟨thm, w7⟩, _⟩ := step7                
+                  thm
 
-#check exploreProofs
+#check @exploreProofs
