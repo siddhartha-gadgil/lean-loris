9b5154579814d95a94b48afd48a3dc2496584ecf
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-04-13 15:37:02 +0530

    regularization, tensorboard: validation  good

--- first_premise_predictor.py
+++ first_premise_predictor.py
@@ -1,19 +1,25 @@
+from matplotlib import pyplot as plt
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
+import tensorflow_docs as tfdocs
+from tensorflow.keras import regularizers
+from tensorflow.keras import layers
+from tensorflow import keras
+import tensorflow as tf
+import json
 file = open(r"data/matrices.json", "r")
 js = file.read()
-import json
 data = json.loads(js)
 file.close()
 names = data["names"]
-print (len(names))
+print(len(names))
 dim = len(names)
 terms = data["terms"]
 types = data["types"]
 size = len(terms)
-print (len(terms))
-print (len(types))
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+print(len(terms))
+print(len(types))
+
 
 # inputs = keras.Input(shape=(dim,), name="types")
 # x = layers.Dense(32, activation="relu", name="dense_1")(inputs)
@@ -23,59 +29,81 @@ from tensorflow.keras import layers
 def floatlist(l):
     return [float(x) for x in l]
 
+
 def normalize(l):
     total = sum(l)
     if total == 0:
         return floatlist(l)
-    return [float(x) / float(total) for x in l] 
+    return [float(x) / float(total) for x in l]
+
 
 statements = [floatlist(row) for row in types]
 proofs = [normalize(row) for row in terms]
 
 model = keras.Sequential([
     keras.Input(shape=(dim,), name="types"),
-    layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_1"),
-    layers.Dense(32, activation="relu", kernel_initializer='glorot_normal',  name="dense_2"),
-    # layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_3"),
-    # layers.Dense(32, activation="relu", kernel_initializer='glorot_normal', name="dense_4"),
-    layers.Dense(dim, activation="softmax", name="predictions"),
-    ]
-    )
+    layers.Dense(32, activation="elu", kernel_initializer='glorot_normal',
+                 kernel_regularizer=regularizers.l2(0.01), name="dense_1"),
+    layers.Dropout(0.5),
+    layers.Dense(32, activation="elu", kernel_initializer='glorot_normal',
+                 kernel_regularizer=regularizers.l2(0.001), name="dense_2"),
+    layers.Dropout(0.5),
+    layers.Dense(dim, activation="softmax", name="predictions")
+]
+)
 
 statements_tensor = tf.convert_to_tensor(statements[:-1000], dtype=tf.float32)
 proofs_tensor = tf.convert_to_tensor(proofs[:-1000], dtype=tf.float32)
+test_statements_tensor = tf.convert_to_tensor(
+    statements[-1000:], dtype=tf.float32)
+test_proofs_tensor = tf.convert_to_tensor(proofs[-1000:], dtype=tf.float32)
 print(statements_tensor.shape)
 print(proofs_tensor.shape)
+
+keras.callbacks.TensorBoard(
+    log_dir="/home/gadgil/code/lean-loris/logs",
+    histogram_freq=0,  # How often to log histogram visualizations
+    embeddings_freq=0,  # How often to log embedding visualizations
+    update_freq="epoch",
+)  # How often to write logs (default: once per epoch)
 
 model.compile(
     optimizer=keras.optimizers.Adam(),  # Optimizer
     # Loss function to minimize
     loss=keras.losses.MeanSquaredError(),
     # List of metrics to monitor
-    # metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    metrics=[keras.metrics.MeanSquaredError()],
 )
 
 print('Built tensors')
 
 print("Fit model on training data")
+
+log_dir = "/home/gadgil/code/lean-loris/logs"
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir, histogram_freq=1)
 history = model.fit(
     statements_tensor,
     proofs_tensor,
     batch_size=64,
-    epochs=1024
+    epochs=16,
     # We pass some validation for
     # monitoring validation loss and metrics
     # at the end of each epoch
+    validation_data=(test_statements_tensor, test_proofs_tensor),
+    callbacks=[tensorboard_callback]
 )
 
 print("Done training")
 
 print(history.history)
 
-print(model.summary()) 
+print(model.summary())
+
 
 def namevec(name):
     return tf.convert_to_tensor([[1.0 if x == name else 0 for x in names]], dtype=tf.float32)
+
 
 def sort_weighted(weights):
     def sort_key(x):
@@ -87,14 +115,17 @@ def sort_weighted(weights):
     w.sort(reverse=True)
     return [(ns[i], w[i]) for i in range(len(names)) if w[i] > 0]
 
+
 def name_prediction_weights(name):
     return sort_weighted(model.predict(namevec(name))[0])
+
 
 def index_prediction(index):
     stat = statements[index]
     stat_tensor = tf.convert_to_tensor([stat], dtype=tf.float32)
     prediction = model.predict(stat_tensor)[0]
     return sort_weighted(stat), sort_weighted(proofs[index]), sort_weighted(prediction)
+
 
 def name_weight(l, name):
     for (n, w) in l:
---
+++ reso.md
@@ -0,0 +1,5 @@
+This is in addition to my comments on the previous material.
+
+The article has interesting material, but is still very long though changes have been made in the light of my previous comments, . This is both an issue in terms of pages taken up and the waning attention of the reader.
+
+It is up to the editors judgement whether an article this lond, and hence with an "interest density" lower than many expository articles, should be published in resonance.
