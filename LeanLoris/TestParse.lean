cd08fc3e05d9ec65029c80169b3e7d22b1896aca
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-06-16 18:34:06 +0530

    some cleanup

--- LeanLoris/TestParse.lean
+++ LeanLoris/TestParse.lean
@@ -2,15 +2,7 @@ import Lean
 import Lean.Meta
 import Lean.Elab
 import Lean.Parser
-import Init.System
 import Std
-import LeanLoris.Utils
-import LeanLoris.ExprPieces
-import LeanLoris.ConstDeps
-import Lean.Data.Json.Basic
-import Lean.Data.Json.Printer
-import Lean.Data.Json.Basic
-import Lean.Data.Json.FromToJson
 import Mathlib.Algebra.Group.Defs
 open Lean Meta Std Elab Parser 
 
@@ -38,20 +30,17 @@ syntax term "⊆" term : term
 
 #eval checkTerm "a • s"
 
-def checkStatements : MetaM (List (String × Bool)) := do
-  let prompts ← depsPrompt
-  (prompts.toList.take 20).mapM fun s => do return (s, ← checkTerm s)
-
-#eval checkStatements
-
 declare_syntax_cat term3
 
 syntax term : term3
 syntax "λ" ident "," term3 : term3
 syntax "λ"  ident ":" term3  "," term3 : term3
+syntax "λ" "(" ident ":" term3 ")" "," term3 : term3
 macro_rules
 | `(term3|$x:term) => `($x)
 | `(term3|λ $x:ident : $type:term , $y:term) => 
+  `(fun ($x : $type)  => $y)
+| `(term3|λ ( $x:ident : $type:term ) , $y:term) => 
   `(fun ($x : $type)  => $y)
 
 def checkTerm3 (s : String) : MetaM Bool := do
@@ -64,17 +53,23 @@ def checkTerm3 (s : String) : MetaM Bool := do
 #eval checkTerm "λ x : Nat, x + 1"
 #eval checkTerm3 "λ x : Nat, x + 1"
 
+def checkStatements : MetaM (List (String × Bool × Bool)) := do
+  let prompts ← depsPrompt
+  (prompts.toList.take 50).mapM fun s => 
+    do return (s, ← checkTerm s, ← checkTerm3 s)
+
+#eval checkStatements
+
 
 syntax(name:= lean3syn) "lean3" term3 : term
 @[termElab lean3syn] def elab3: Term.TermElab := fun s typ? =>  
   do
   match s with
-  | `(lean3 $x:term) => do 
+  | `(lean3 $x:term3) => do 
       Term.elabTerm x typ?
-  | `(lean3 λ $x:ident : $type:term3 , $y:term3) => 
-      let stx ←  `(fun ($x : $type)  => $y)
-      Term.elabTerm stx typ?
   | _ => throwIllFormedSyntax 
 
 def eg3 := lean3 λ x : Nat, x + 1
+#print eg3
+def eg3' := lean3 λ (x : Nat), x + 1
 #print eg3
