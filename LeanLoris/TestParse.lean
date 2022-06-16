8ae6480901e50edb71e4769f62d975abc1509fa1
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-06-16 17:28:11 +0530

    some term3 parsing

--- LeanLoris/TestParse.lean
+++ LeanLoris/TestParse.lean
@@ -1,5 +1,6 @@
 import Lean
 import Lean.Meta
+import Lean.Elab
 import Lean.Parser
 import Init.System
 import Std
@@ -42,3 +43,38 @@ def checkStatements : MetaM (List (String × Bool)) := do
   (prompts.toList.take 20).mapM fun s => do return (s, ← checkTerm s)
 
 #eval checkStatements
+
+declare_syntax_cat term3
+
+syntax term : term3
+syntax "λ" ident "," term3 : term3
+syntax "λ"  ident ":" term3  "," term3 : term3
+macro_rules
+| `(term3|$x:term) => `($x)
+| `(term3|λ $x:ident : $type:term , $y:term) => 
+  `(fun ($x : $type)  => $y)
+
+def checkTerm3 (s : String) : MetaM Bool := do
+  let env ← getEnv
+  let chk := runParserCategory env `term3  s
+  match chk with
+  | Except.ok _  => pure true
+  | Except.error _  => pure false
+
+#eval checkTerm "λ x : Nat, x + 1"
+#eval checkTerm3 "λ x : Nat, x + 1"
+
+
+syntax(name:= lean3syn) "lean3" term3 : term
+@[termElab lean3syn] def elab3: Term.TermElab := fun s typ? =>  
+  do
+  match s with
+  | `(lean3 $x:term) => do 
+      Term.elabTerm x typ?
+  | `(lean3 λ $x:ident : $type:term3 , $y:term3) => 
+      let stx ←  `(fun ($x : $type)  => $y)
+      Term.elabTerm stx typ?
+  | _ => throwIllFormedSyntax 
+
+def eg3 := lean3 λ x : Nat, x + 1
+#print eg3
