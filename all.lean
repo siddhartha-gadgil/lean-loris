baa8c9b094a3e0f73ac0a81800cfeae961246253
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-06-19 11:01:27 +0530

    mild refactoring

--- all.lean
+++ all.lean
@@ -79,7 +79,7 @@ def mathPrompt(mathenv: Environment) : IO Unit := do
   IO.println "\n# Propmpts for premises for machine learning.\n"
   IO.println "We consider dependencies in MathLib4 and generate data for simple prompts. As of now these are for basic experiments.\n"
   let promptCore := prompCoreJs
-  let ei := promptCore.run' 
+  let ei := promptCore.run'   
       {fileName := "", fileMap := ⟨"", #[], #[]⟩, maxHeartbeats := 100000000000, maxRecDepth := 1000000} {env := mathenv}
   match ←  ei.toIO' with
   | Except.ok view => 
@@ -108,28 +108,6 @@ def runLclConst(env: Environment) : IO Unit := do
     do
           let msg ← e.toMessageData.toString
           IO.println msg
-
-def czslDocs : String := 
-"Our main model problem for forward reasoning is the following from a Czech-Slovak Olympiad:
-
-Let M be a set with a product. Given the axioms:
-* ∀ a b : M, (a * b) * b = a
-* ∀ a b : M, a * (a * b) = b
-then, for arbitrary elements m and n in M, we have m * n = n * m.
-
-We fix m and n and reason forward starting with m, n, the axioms, and multiplication. Our forward reasoning is tuned for this problem, and also mildly help by including m *n in the initial state. But we do not use the statement of the problem, any of the lemmas or the proof.
-
-To understand the (automated) reasoning steps (and for use during tuning and debugging), some lemmas and the theorem were defined. While running progress in proving these is logged.
-
-* def lem1! := (m * n) * n = m 
-* def lem2! := (m * n) * ((m * n) * n) = n
-* def lem3! := ((m * n) * m) * m = m * n
-* def lem4! := (m * n) * ((m * n) * n) = (m * n) * m
-* def lem5! := (m * n) * m = n
-* def lem6! := ((m * n) * m) * m = n * m
-* def thm! := m * n = n * m
-
-The progress in proving the lemmas is output at each intermediate step, and finally the proof obtained for the theorem. Running takes a couple of minutes (more on a slower machine).\n"
 
 def runCzSl(env: Environment) : IO Unit := do
   IO.println s!"\n# Czech-Slovak Olympiad example\n\n{czslDocs}"
